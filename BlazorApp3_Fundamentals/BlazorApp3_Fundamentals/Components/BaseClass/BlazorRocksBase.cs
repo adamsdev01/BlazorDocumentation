dedacd721e0c2d32cdaea91f0055f9fd3ceaf420
Adams-Developer <tadams03@mail.roosevelt.edu>
2023-05-19 20:37:03 -0500

    Blazor component examples

---
+++ BlazorApp3_Fundamentals/BlazorApp3_Fundamentals/Components/BaseClass/BlazorRocksBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp3_Fundamentals.Components.BaseClass
+{
+    public class BlazorRocksBase : ComponentBase
+    {
+        public string BlazorRocksText { get; set; } = "Blazor rocks the browser";
+    }
+}
---
+++ BlazorApp3_Fundamentals/BlazorApp3_Fundamentals/Components/ComponentParameters/PanelBody.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp3_Fundamentals.Components.ComponentParameters
+{
+    public class PanelBody
+    {
+        public string? Text { get; set; }
+        public string? Style { get; set; }
+    }
+}
